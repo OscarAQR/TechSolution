2c4ac5b544fdd477ad04a9e22dc8dadbdc768b66
OscarAQR <oaquintanillarodriguez@gmail.com>
2023-09-30 17:25:51 -0600

    first commit

---
+++ src/test/java/com/techsolution/SistemaGestionEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
